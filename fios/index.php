e6fbad71274372a2bc47274322647a05c5ef9e0d
daboyu <53033794+daboyu@users.noreply.github.com>
2020-02-21 22:22:48 +0800

    Update index.php

--- fios/index.php
+++ fios/index.php
@@ -7,7 +7,7 @@
             var URL = 'https://notify-bot.line.me/oauth/authorize?';
             URL += 'response_type=code';
             URL += '&client_id=yl9TKHqCaSDrqG0d3U1Anq';
-            URL += '&redirect_uri=http://127.0.0.1:8080/index.php';
+            URL += '&redirect_uri=http://dabo.xyz/fios/index.php';
             URL += '&scope=notify';
             URL += '&state=abcde';
             window.location.href = URL;
